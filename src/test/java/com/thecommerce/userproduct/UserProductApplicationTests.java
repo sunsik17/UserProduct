9392a40db926b28708902ab5c7cdbf80afdf1eba
sunsik17 <sunsig21c@gmail.com>
2024-04-24 18:48:33 +0900

    Initial commit

---
+++ src/test/java/com/thecommerce/userproduct/UserProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecommerce.userproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
