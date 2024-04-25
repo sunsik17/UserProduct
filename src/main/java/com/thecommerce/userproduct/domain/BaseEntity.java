a74ef46d98690a5b0885bc4111eeabda1682d324
sunsik17 <sunsig21c@gmail.com>
2024-04-25 17:43:09 +0900

    [feat/users-register-1] 회원가입 기능 개발
    - BaseEntity 생성
      - 모든 Entity에서 중복으로 사용 될 컬럼 BaseEntity 생성

---
+++ src/main/java/com/thecommerce/userproduct/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.thecommerce.userproduct.domain;
+
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+	@LastModifiedDate
+	private LocalDateTime updateAt;
+}
