ad07d0fdd18dbe4533a2412ee552ca594d09f147
sunsik17 <sunsig21c@gmail.com>
2024-04-26 02:34:35 +0900

    [feat/user-modify] 회원 수정 기능 구현
    - dto
      - UpdateUser(닉네임, 모바일, 이메일 변경 가능)
      - 바꿀 값이 null이면 기존 값 사용
    
    - etc
      - errorCode 추가

--- src/main/java/com/thecommerce/userproduct/controller/user/UserController.java
+++ src/main/java/com/thecommerce/userproduct/controller/user/UserController.java
@@ -1,16 +1,21 @@
 package com.thecommerce.userproduct.controller.user;
 
 import com.thecommerce.userproduct.domain.user.dto.RegisterUser;
+import com.thecommerce.userproduct.domain.user.dto.UpdateUser;
 import com.thecommerce.userproduct.service.user.UserRegisterService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "회원 등록 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
@@ -25,6 +30,16 @@ public class UserController {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(
 			RegisterUser.Response.from(userRegisterService.join(request))
+		);
+	}
+
+	@PutMapping("/{userId}")
+	public ResponseEntity<UpdateUser.Response> updateRegister(
+		@RequestBody UpdateUser.Request request,
+		@PathVariable String userId) {
+
+		return ResponseEntity.ok().body(
+			UpdateUser.Response.from(userRegisterService.update(userId, request))
 		);
 	}
 }
--- src/main/java/com/thecommerce/userproduct/domain/user/entity/User.java
+++ src/main/java/com/thecommerce/userproduct/domain/user/entity/User.java
@@ -1,6 +1,9 @@
 package com.thecommerce.userproduct.domain.user.entity;
 
+import static com.thecommerce.userproduct.exception.contants.ErrorCode.NEED_TO_CHECK_THE_NICKNAME_LENGTH;
+
 import com.thecommerce.userproduct.domain.BaseEntity;
+import com.thecommerce.userproduct.exception.UserServiceException;
 import javax.persistence.Entity;
 import javax.validation.constraints.Email;
 import lombok.AllArgsConstructor;
@@ -21,4 +24,18 @@ public class User extends BaseEntity {
 	private String mobileNumber;
 	@Email
 	private String email;
+
+	public void update(String newNickname, String newMobileNum, String newEmail) {
+		validateChangeNickname(newNickname);
+		this.nickname = newNickname == null ? nickname : newNickname;
+		this.mobileNumber = newMobileNum == null ? mobileNumber : newMobileNum;
+		this.email = newEmail == null ? email : newEmail;
+	}
+
+	private void validateChangeNickname(String nickname) {
+		if (nickname == null) return;
+		if (nickname.length() < 1 || nickname.length() > 6) {
+			throw new UserServiceException(NEED_TO_CHECK_THE_NICKNAME_LENGTH);
+		}
+	}
 }
